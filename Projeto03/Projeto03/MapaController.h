29eb8f7a767e1cb7a01d07d8a8c07de5fc4cc869
Tutz <ttutty.oliveira@gmail.com>
2014-03-10 17:02:57 -0300

    Colocando o mapa no MapaController

---
+++ Projeto03/Projeto03/MapaController.h
@@ -0,0 +1,15 @@
+//
+//  MapaController.h
+//  Projeto03
+//
+//  Created by ARTHUR HENRIQUE VIEIRA DE OLIVEIRA on 10/03/14.
+//  Copyright (c) 2014 ARTHUR HENRIQUE VIEIRA DE OLIVEIRA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MapaController : ViewController
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapa;
+
+@end
---
+++ Projeto03/Projeto03/MapaController.m
@@ -0,0 +1,40 @@
+//
+//  MapaController.m
+//  Projeto03
+//
+//  Created by ARTHUR HENRIQUE VIEIRA DE OLIVEIRA on 10/03/14.
+//  Copyright (c) 2014 ARTHUR HENRIQUE VIEIRA DE OLIVEIRA. All rights reserved.
+//
+
+#import "MapaController.h"
+
+@interface MapaController ()
+
+@end
+
+@implementation MapaController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+} 
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self setMapa:[[MKMapView alloc] initWithFrame:CGRectMake(0, 30, 400, 600)]];
+    [[self view] addSubview:[self mapa]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
