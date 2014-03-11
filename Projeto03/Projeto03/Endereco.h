fe82523ffe47d36180ce2b4bf1926229fcf414ce
Tutz <ttutty.oliveira@gmail.com>
2014-03-11 16:03:19 -0300

    colocando o objeto endereco

---
+++ Projeto03/Projeto03/Endereco.h
@@ -0,0 +1,13 @@
+//
+//  Endereco.h
+//  Projeto03
+//
+//  Created by ARTHUR HENRIQUE VIEIRA DE OLIVEIRA on 11/03/14.
+//  Copyright (c) 2014 ARTHUR HENRIQUE VIEIRA DE OLIVEIRA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Endereco : NSObject
+
+@end
---
+++ Projeto03/Projeto03/Endereco.m
@@ -0,0 +1,13 @@
+//
+//  Endereco.m
+//  Projeto03
+//
+//  Created by ARTHUR HENRIQUE VIEIRA DE OLIVEIRA on 11/03/14.
+//  Copyright (c) 2014 ARTHUR HENRIQUE VIEIRA DE OLIVEIRA. All rights reserved.
+//
+
+#import "Endereco.h"
+
+@implementation Endereco
+
+@end
