39978394e9bedc8fb79d683d53cc04803e0004a5
Tutz <ttutty.oliveira@gmail.com>
2014-03-11 09:52:13 -0300

    Criando os Controllers

---
+++ Projeto03/Projeto03/InfoDeRoleController.h
@@ -0,0 +1,13 @@
+//
+//  InfoDeRoleController.h
+//  Projeto03
+//
+//  Created by Arthur Henrique Vieira de Oliveira on 11/03/14.
+//  Copyright (c) 2014 ARTHUR HENRIQUE VIEIRA DE OLIVEIRA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface InfoDeRoleController : ViewController
+
+@end
