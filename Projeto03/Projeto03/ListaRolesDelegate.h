1d8102ad968ee600c47d166d36a6f4c5f0b48beb
Luiz Fernando <luizinho_mack@yahoo.com.br>
2014-03-16 12:51:36 -0300

    Criado um delegate para notificar mudanças no estado da ListaRoles

---
+++ Projeto03/Projeto03/ListaRolesDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ListaRolesDelegate.h
+//  Projeto03
+//
+//  Created by Luiz Fernando Silva on 16/03/14.
+//  Copyright (c) 2014 ARTHUR HENRIQUE VIEIRA DE OLIVEIRA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class ListaRoles;
+@class Role;
+// Delegate que recebe eventos da classe ListaRoles
+@protocol ListaRolesDelegate <NSObject>
+
+- (void)listaRole:(ListaRoles*)lista atualizouEnderecoDe:(Role*)role;
+- (void)listaRole:(ListaRoles*)lista adicionouRole:(Role*)role;
+
+@end
